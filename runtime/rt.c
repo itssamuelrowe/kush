d241e825ac5cc40e5422b60ea3f5a62258c9e524
Samuel Rowe <samuelrowe1999@gmail.com>
2022-07-23 23:47:41 +0530

    feat: implemented `ksToCString` and `ksStringGetSize` runtime functions

--- runtime/rt.c
+++ runtime/rt.c
@@ -3,7 +3,10 @@
 #include <stdlib.h>
 
 #define ARRAY_HEADER_SIZE 4
-#define afterHeader(pointer) ((uint8_t*)(pointer) + ARRAY_HEADER_SIZE)
+
+#define STRING_HEADER_SIZE 4
+
+#define ksArrayAfterHeader(pointer) ((uint8_t*)(pointer) + ARRAY_HEADER_SIZE)
 
 uint8_t* ksAllocateArray(int32_t itemSize, int32_t itemCount) {
    uint8_t* result = malloc(ARRAY_HEADER_SIZE + (itemSize * itemCount));
@@ -13,13 +16,24 @@ uint8_t* ksAllocateArray(int32_t itemSize, int32_t itemCount) {
 }
 
 int32_t ksArrayGetI32(int8_t* array, int32_t index) {
-   return ((int32_t*)afterHeader(array))[index];
+   return ((int32_t*)ksArrayAfterHeader(array))[index];
 }
 
 int32_t* ksArrayGetPointerI32(int8_t* array, int32_t index) {
-   return &((int32_t*)afterHeader(array))[index];
+   return &((int32_t*)ksArrayAfterHeader(array))[index];
 }
 
 int32_t ksArrayGetSize(int8_t* array) {
    return ((int32_t*)array)[0];
+}
+
+const uint8_t* ksToCString(const uint8_t* value) {
+   return (((uint8_t*)(value)) + STRING_HEADER_SIZE);
+}
+
+int32_t ksStringGetSize(const uint8_t* string) {
+   return (string[0] << 6 |
+      string[1] << 4 |
+      string[2] << 2 |
+      string[3] << 0);
 }
